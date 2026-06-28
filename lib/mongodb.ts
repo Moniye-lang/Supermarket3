0bc89367a90f3b912c05b02c8d936a20fe5089df
Moniye-lang <davidadeniyi269@gmail.com>
2026-06-28 17:22:36 +0100

    Import all schemas in mongodb.ts to prevent MissingSchemaError on Vercel populates

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,4 +1,9 @@
 import mongoose from "mongoose";
+import "@/lib/models/User";
+import "@/lib/models/Product";
+import "@/lib/models/Order";
+import "@/lib/models/Cart";
+import "@/lib/models/Tracking";
 
 const MONGODB_URI = process.env.MONGO_URI || process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/amstores";
 
